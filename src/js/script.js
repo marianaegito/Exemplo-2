7dcd3a96d7684e38d0e87d3cd80bf9212608ba69
Mariana <marianaegito2007@gmail.com>
2025-04-01 09:11:21 -0300

    Declarando variaveis

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome="fiap";
+console.log(nome)
+
+let idade=17;
+console.log(idade)
+
+const sobrenome ="Egito";
+console.log(sobrenome)
+//underfined
+let aula; 
+console.log(aula)
+//underfined
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
+
+
+
+
+
+
