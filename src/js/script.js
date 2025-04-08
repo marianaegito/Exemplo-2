0db53798cbbc9d0c5fb208078651c91d44a1c7d9
Mariana <marianaegito2007@gmail.com>
2025-04-08 09:15:21 -0300

    Adicionando estruturas condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,100 +1,123 @@
 //DECLARAÇÕES E VARIAVEIS
-var nome="fiap";
-console.log(nome)
+// var nome="fiap";
+// console.log(nome)
 
-let idade=17;
-console.log(idade)
+// let idade=17;
+// console.log(idade)
 
-const sobrenome ="Egito";
-console.log(sobrenome)
-//underfined
-let aula; 
-console.log(aula)
-//underfined
-let valor = null;
-console.log(valor)
+// const sobrenome ="Egito";
+// console.log(sobrenome)
+// //underfined
+// let aula; 
+// console.log(aula)
+// //underfined
+// let valor = null;
+// console.log(valor)
 
-let exemplo1={};
+// let exemplo1={};
 
-let exemplo2=[];
+// let exemplo2=[];
 
-//TIPO VARIAVEIS
-let exemplo3 = 10;
-console.log(typeof exemplo3)
+// //TIPO VARIAVEIS
+// let exemplo3 = 10;
+// console.log(typeof exemplo3)
 
-let exemplo4 = "Aula"
-console.log(typeof exemplo4)
+// let exemplo4 = "Aula"
+// console.log(typeof exemplo4)
 
-let exemplo5 = true
-console.log(typeof exemplo5)
+// let exemplo5 = true
+// console.log(typeof exemplo5)
 
-let exemplo6 =["huguinho","zezinho","luizinho"]
-console.log(typeof exemplo6)
+// let exemplo6 =["huguinho","zezinho","luizinho"]
+// console.log(typeof exemplo6)
 
 
-//CONVERSÕES
+// //CONVERSÕES
 
-//float => inteiro
+// //float => inteiro
 
-let numfloat =123.456;
-console.log(parseInt(numfloat));
+// let numfloat =123.456;
+// console.log(parseInt(numfloat));
 
-//string => float
-let numString ="547.987";
-console.log(parseFloat(numString))
+// //string => float
+// let numString ="547.987";
+// console.log(parseFloat(numString))
 
-//float => string
+// //float => string
 
-let numfloat1= 554.665;
-console.log(numfloat1.toString());
+// let numfloat1= 554.665;
+// console.log(numfloat1.toString());
 
-let numInt=100;
-console.log(numInt.toString())
+// let numInt=100;
+// console.log(numInt.toString())
 
-//METODOD - PARTE-1
+// //METODOD - PARTE-1
 
-//LENGTH - VERIFICA O TAMANHO DA STRING
+// //LENGTH - VERIFICA O TAMANHO DA STRING
 
-let frase="O mundo da Tecnologia";
-console.log(frase.length)
+// let frase="O mundo da Tecnologia";
+// console.log(frase.length)
 
-//indexOf - retorna um trecho de um txto
+// //indexOf - retorna um trecho de um txto
 
-let texto = "Progamação Sustentável"
-console.log(texto.indexOf("a"))
+// let texto = "Progamação Sustentável"
+// console.log(texto.indexOf("a"))
 
-//slice - retorna parte de um texto apontando o inicio e o final
+// //slice - retorna parte de um texto apontando o inicio e o final
 
-let info= "processamento de ponta";
-console.log(info.slice(0,13))
+// let info= "processamento de ponta";
+// console.log(info.slice(0,13))
 
-// OPERADORES ARITMÉTICOS
+// // OPERADORES ARITMÉTICOS
 
-const num1=10;
-const num2=20;
+// const num1=10;
+// const num2=20;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
 
 //OPERADORES LÓGICOS
 
-const num3=20;
-const num4=30;
+// const num3=20;
+// const num4=30;
 
-console.log(num3 < num4)
-console.log(num3 > num4 && num4 < 50)
-console.log(num3 > num4 || num4 > num3)
-console.log(num4 == num3 || num4 <= num3)
+// console.log(num3 < num4)
+// console.log(num3 > num4 && num4 < 50)
+// console.log(num3 > num4 || num4 > num3)
+// console.log(num4 == num3 || num4 <= num3)
 
-// OPERADORES DE COMPARAÇÃO
-const num5 = 25;
-const num6 =35;
-console.log(num5 == num6); //compara
-console.log(num5 === num6); //compara e veifica o tipo de variavel
-console.log(num5 != num6); //diferente
 
+// IF
+
+// let valor =100;
+
+
+// if(valor == 100)(
+//     console.log("é verdadeiro")
+// )
+
+// //if/else
+
+// let valor1 = 100;
+
+// if(valor1 == 100){
+//     console.log("Valor Correto")
+// }else{
+//     console.log("Valor Errado")
+// }
+
+//if/else - encadeado ou aninhado
+
+// 
+
+// CONDIÇÃO TERNARIA
+
+let valor2= 300;
+
+let resultado = valor2 == 300 ? "Certo": "Errado"
+console.log(resultado)
 
 
 
